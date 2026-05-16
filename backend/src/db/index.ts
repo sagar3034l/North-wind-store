d2f7b5e9f08c7815ef9e96a5d75021c4787e8f51
sagar3034l <sd8778610@gmail.com>
2026-05-16 21:09:19 +0530

    fix: update import path for schema to use .js extension

--- backend/src/db/index.ts
+++ backend/src/db/index.ts
@@ -1,7 +1,7 @@
 import "dotenv/config"
 import { drizzle } from "drizzle-orm/node-postgres";
 import pg from 'pg'
-import *as schema from './schema'
+import *as schema from './schema.js'
 
 const pool = new pg.Pool({connectionString:process.env.DATABASE_URL})
 
